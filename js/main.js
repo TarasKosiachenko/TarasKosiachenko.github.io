905fe9ef872f56dea37b94d2f6582ece67cb2439
Taras Kosiachenko <t.kosiachenko@gmail.com>
2020-06-01 20:41:24 +0300

    complete portfolio

--- js/main.js
+++ js/main.js
@@ -12,4 +12,16 @@ document.querySelector(".header__burger").onclick = () => {
     document.querySelector(".header__burger").classList.toggle("show");
     document.querySelector("body").classList.toggle("lock");
   };
-  
+
+
+var btn = document.getElementById('btnshow');
+    btn.onclick = function(e) {
+      var element = document.querySelector(".hidden");
+      if(element.classList.contains("show-hidden")) {
+    element.classList.remove("show-hidden");
+    btn.innerHTML = "Show More";
+    } else {
+      element.classList.add("show-hidden");
+      btn.innerHTML = "Close";
+    }
+}
